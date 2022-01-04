f525881f087300544e753e40f703aa7d701ace80
Trần Phúc Thành <enigma15012001@gmail.com>
2022-01-04 11:52:24 +0700

    1

--- files/permu.c
+++ files/permu.c
@@ -6,10 +6,10 @@ int main()
 
     int count = 0;
     
-    for (int i = 0; i <= 14; i++){
-        for (int j = 0; j <= 14; j++){
-            for (int k = 0; k <= 14; k++){
-                if (i+ 2*j + 5*k == 14)
+    for (int i = 0; i <= 11; i++){
+        for (int j = 0; j <= 11; j++){
+            for (int k = 0; k <= 11; k++){
+                if (i+ j + k == 11)
                     {
                         count++;
                         printf("%d %d %d\n", i, j, k);
